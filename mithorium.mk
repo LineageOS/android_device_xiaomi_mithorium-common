d9cdff2680c9c93f2144ba8e5b42b808fa77d706
Yumi Yukimura <me.cafebabe@gmail.com>
2024-05-27 15:25:16 +0800

    [WIP] mithorium-common: Build & Configure external camera provider aidl
    Change-Id: Ic964dd771f12055145b7970649c93901bc34e57b

--- mithorium.mk
+++ mithorium.mk
@@ -133,6 +133,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++_vendor
 
+# Camera (External)
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider-V1-external-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_led \
