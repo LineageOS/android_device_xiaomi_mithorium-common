4d4cb4ada263b7878cbc92289fb1b09499bee177
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:15:21 +0800

    mithorium-common: memtrack: Convert to AIDL HAL
    Change-Id: I5b222d8b40f7d3bc77458862045a5793dea47cdd

--- memtrack/kgsl.cpp
+++ memtrack/kgsl.cpp
@@ -20,47 +20,33 @@
 #include <string.h>
 #include <sys/mman.h>
 
-#include <hardware/memtrack.h>
-
 #include "memtrack_msm.h"
 
-#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
-#define min(x, y) ((x) < (y) ? (x) : (y))
+using namespace aidl::android::hardware::memtrack;
 
-struct memtrack_record record_templates[] = {
+static class MemtrackRecord record_templates[] = {
     {
-        .flags = MEMTRACK_FLAG_SMAPS_ACCOUNTED |
-                 MEMTRACK_FLAG_PRIVATE |
-                 MEMTRACK_FLAG_NONSECURE,
+        .flags = MemtrackRecord::FLAG_SMAPS_ACCOUNTED |
+                 MemtrackRecord::FLAG_PRIVATE |
+                 MemtrackRecord::FLAG_NONSECURE,
     },
     {
-        .flags = MEMTRACK_FLAG_SMAPS_UNACCOUNTED |
-                 MEMTRACK_FLAG_PRIVATE |
-                 MEMTRACK_FLAG_NONSECURE,
+        .flags = MemtrackRecord::FLAG_SMAPS_UNACCOUNTED |
+                 MemtrackRecord::FLAG_PRIVATE |
+                 MemtrackRecord::FLAG_NONSECURE,
     },
 };
 
-int kgsl_memtrack_get_memory(pid_t pid, enum memtrack_type type,
-                             struct memtrack_record *records,
-                             size_t *num_records)
+int kgsl_memtrack_get_memory(int pid, MemtrackType type,
+                             std::vector<MemtrackRecord>* _aidl_return)
 {
-    size_t allocated_records = min(*num_records, ARRAY_SIZE(record_templates));
     char syspath[128];
-    size_t accounted_size = 0;
-    size_t unaccounted_size = 0;
+    long accounted_size = 0;
+    long unaccounted_size = 0;
     FILE *fp;
     int ret;
 
-    *num_records = ARRAY_SIZE(record_templates);
-
-    /* fastpath to return the necessary number of records */
-    if (allocated_records == 0)
-        return 0;
-
-    memcpy(records, record_templates,
-            sizeof(struct memtrack_record) * allocated_records);
-
-    if (type == MEMTRACK_TYPE_GL) {
+    if (type == MemtrackType::GL) {
 
         snprintf(syspath, sizeof(syspath),
                  "/sys/class/kgsl/kgsl/proc/%d/gpumem_mapped", pid);
@@ -69,7 +55,7 @@ int kgsl_memtrack_get_memory(pid_t pid, enum memtrack_type type,
         if (fp == NULL)
             return -errno;
 
-        ret = fscanf(fp, "%zu", &accounted_size);
+        ret = fscanf(fp, "%lu", &accounted_size);
         if (ret != 1) {
             fclose(fp);
             return -EINVAL;
@@ -85,14 +71,14 @@ int kgsl_memtrack_get_memory(pid_t pid, enum memtrack_type type,
             return -errno;
         }
 
-        ret = fscanf(fp, "%zu", &unaccounted_size);
+        ret = fscanf(fp, "%lu", &unaccounted_size);
         if (ret != 1) {
             fclose(fp);
             return -EINVAL;
         }
         fclose(fp);
 
-    } else if (type == MEMTRACK_TYPE_GRAPHICS) {
+    } else if (type == MemtrackType::GRAPHICS) {
 
         snprintf(syspath, sizeof(syspath),
                  "/sys/class/kgsl/kgsl/proc/%d/imported_mem", pid);
@@ -101,7 +87,7 @@ int kgsl_memtrack_get_memory(pid_t pid, enum memtrack_type type,
         if (fp == NULL)
             return -errno;
 
-        ret = fscanf(fp, "%zu", &unaccounted_size);
+        ret = fscanf(fp, "%lu", &unaccounted_size);
         if (ret != 1) {
             fclose(fp);
             return -EINVAL;
@@ -109,11 +95,10 @@ int kgsl_memtrack_get_memory(pid_t pid, enum memtrack_type type,
         fclose(fp);
     }
 
-    if (allocated_records > 0)
-    records[0].size_in_bytes = accounted_size;
-
-    if (allocated_records > 1)
-    records[1].size_in_bytes = unaccounted_size;
+    record_templates[0].sizeInBytes = accounted_size;
+    _aidl_return->emplace_back(record_templates[0]);
+    record_templates[1].sizeInBytes = unaccounted_size;
+    _aidl_return->emplace_back(record_templates[1]);
 
     return 0;
 }
