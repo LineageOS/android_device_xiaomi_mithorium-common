6a5c92525a2fc56a8a73259ceae5961ae6b44287
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    mithorium-common: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    [cafebabe] Adapt
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- mithorium.mk
+++ mithorium.mk
@@ -469,5 +469,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wifi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_WCNSS_qcom_wlan_nv.bin_symlink \
+    firmware_WCNSS_wlan_dictionary.dat_symlink
+
+ifneq ($(TARGET_OVERRIDE_WLAN_MAC_BIN_SYMLINK),true)
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink
+endif
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/mithorium-common/mithorium-common-vendor.mk)
