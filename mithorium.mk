a87ffb64ea5439227c28015a6086e92f92039436
Bruno Martins <bgcngm@gmail.com>
2026-06-27 00:03:32 +0100

    mithorium-common: Remove disable_configstore from PRODUCT_PACKAGES
    Configstore is dead in CP2A.
    
    Change-Id: I8efc0e2171c2841fa888d8b11b33691f955bd36d

--- mithorium.mk
+++ mithorium.mk
@@ -136,10 +136,6 @@ PRODUCT_PACKAGES += \
     charger_led \
     charger_led_recovery
 
-# Configstore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Consumer IR
 ifneq ($(TARGET_HAS_NO_CONSUMERIR),true)
 PRODUCT_PACKAGES += \
